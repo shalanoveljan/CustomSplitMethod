15fb99b1b342689adc1c213b4eab52367ce7fec7
shalanoveljan <eljanash@code.edu.az>
2023-08-07 08:46:39 +0400

    CustomSplit

---
+++ CustomSplitMethod/CustomClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSplitMethod
+{
+    internal static class CustomClass
+    {
+        public static string[] CustomSplit(this string text, char letter)
+        {
+            int count = 0;
+            string[] NewText = new string[0];
+            char[] letters = text.ToCharArray();
+            Array.Resize(ref letters, letters.Length + 1);
+            for (int i = 0; i <letters.Length; i++)
+            {
+                if (letters[i] == letter || letters[i]==default(char))
+                {
+                    Array.Resize(ref NewText, NewText.Length + 1);
+                    NewText[NewText.Length - 1] = new string(letters).Substring(0, count);
+
+                    letters = new string(letters).Substring(count + 1).ToCharArray();
+                    count = 0;
+                    i = -1;
+
+                }
+                // "Elcan Bakidan Gelir"
+                else
+                {
+                    count++;
+
+
+                }
+            }
+            return NewText;
+
+        }
+
+        public static string[] CustomSplit(this string text)
+        {
+            int count = 0;
+            string[] NewText = new string[0];
+            char[] letters = text.ToCharArray();
+            Array.Resize(ref letters, letters.Length + 1);
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] ==' ' || letters[i] == default(char))
+                {
+                    Array.Resize(ref NewText, NewText.Length + 1);
+                    NewText[NewText.Length - 1] = new string(letters).Substring(0, count);
+
+                    letters = new string(letters).Substring(count + 1).ToCharArray();
+                    count = 0;
+                    i = -1;
+
+                }
+                // "Elcan Bakidan Gelir"
+                else
+                {
+                    count++;
+
+
+                }
+            }
+            return NewText;
+
+        }
+
+
+    }
+}
+
---
+++ CustomSplitMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using CustomSplitMethod;
+
+string text = "Elcan Bakidan Gelir";
+
+char letter = 'n';
+
+foreach (var item in text.CustomSplit(letter))
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("------------------");
+
+foreach (var item in text.CustomSplit())
+{
+    Console.WriteLine(item);
+}
